116227373ccf83fefd7f40ba87c3823525082c4b
ohshitnotgood <praanto@icloud.com>
2025-04-23 00:35:22 +0200

    changed port nr

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import express from 'express'
 import { v2Router } from './v2/v2router'
 import { v1Router } from './v1/v1router'
 
-const app = express()
-const port = 8000
+export const app = express()
+const port = 3000
 
 app.use("/v1", v1Router)
 app.use("/v2/", v2Router)
