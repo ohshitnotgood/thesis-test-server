23b665956d13d23eec2f51a0fc0eb16dcdea81de
ohshitnotgood <praanto@icloud.com>
2025-04-23 13:09:22 +0200

    added cors

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 import { v2Router } from './v2/v2router'
 import { v1Router } from './v1/v1router'
+import cors from "cors"
 
 export const app = express()
 const port = 3000
+
+const corsOptions = {
+    origin: ['https://test-frontend-ssr.vercel.app']
+}
+
+app.use(cors(corsOptions))
 
 app.use("/v1", v1Router)
 app.use("/v2/", v2Router)
